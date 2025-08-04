5d1ad8bc87f8968fcc66b135bb34fed904ef1c79
Owilii <henryonyango506@gmail.com>
2025-08-04 08:37:59 +0300

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,590 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec-section-sets-and-elements",
+  "level": "1",
+  "url": "sec-section-sets-and-elements.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Sets and Elements",
+  "body": " Sets and Elements    "
+},
+{
+  "id": "sec-section-set-description",
+  "level": "1",
+  "url": "sec-section-set-description.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Set Description",
+  "body": " Set Description    "
+},
+{
+  "id": "sec-section-the-notation-of-sets",
+  "level": "1",
+  "url": "sec-section-the-notation-of-sets.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "The Notation of Sets",
+  "body": " The Notation of Sets  The  "
+},
+{
+  "id": "sec-section-operations-on-sets",
+  "level": "1",
+  "url": "sec-section-operations-on-sets.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Operations on Sets",
+  "body": " Operations on Sets    "
+},
+{
+  "id": "sec-section-application",
+  "level": "1",
+  "url": "sec-section-application.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Application",
+  "body": " Application    "
+},
+{
+  "id": "sec-section-summary1",
+  "level": "1",
+  "url": "sec-section-summary1.html",
+  "type": "Section",
+  "number": "1.6",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise1",
+  "level": "1",
+  "url": "sec-section-review-exercise1.html",
+  "type": "Section",
+  "number": "1.7",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "sec-section-revision-on-natural-numbers-and-integers",
+  "level": "1",
+  "url": "sec-section-revision-on-natural-numbers-and-integers.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Revision on Natural Numbers and Integers",
+  "body": " Revision on Natural Numbers and Integers    "
+},
+{
+  "id": "sec-section-rational-numbers",
+  "level": "1",
+  "url": "sec-section-rational-numbers.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Rational Numbers",
+  "body": " Rational Numbers    "
+},
+{
+  "id": "sec-section-irrational-numbers",
+  "level": "1",
+  "url": "sec-section-irrational-numbers.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Irrational Numbers",
+  "body": " Irrational Numbers    "
+},
+{
+  "id": "sec-section-real-numbers",
+  "level": "1",
+  "url": "sec-section-real-numbers.html",
+  "type": "Section",
+  "number": "2.4",
+  "title": "Real Numbers",
+  "body": " Real Numbers    "
+},
+{
+  "id": "sec-section-application1",
+  "level": "1",
+  "url": "sec-section-application1.html",
+  "type": "Section",
+  "number": "2.5",
+  "title": "Application",
+  "body": " Application    "
+},
+{
+  "id": "sec-section-summary2",
+  "level": "1",
+  "url": "sec-section-summary2.html",
+  "type": "Section",
+  "number": "2.6",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise2",
+  "level": "1",
+  "url": "sec-section-review-exercise2.html",
+  "type": "Section",
+  "number": "2.7",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "sec-section-revision-on-linear-equation-in-one-variable",
+  "level": "1",
+  "url": "sec-section-revision-on-linear-equation-in-one-variable.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Revision on Linear Equation in One Variable",
+  "body": " Revision on Linear Equation in One Variable    "
+},
+{
+  "id": "sec-section-systems-of-linear-equations-in-two-variables",
+  "level": "1",
+  "url": "sec-section-systems-of-linear-equations-in-two-variables.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Systems of Linear Equations in Two Variables",
+  "body": " Systems of Linear Equations in Two Variables    "
+},
+{
+  "id": "sec-section-solving-non-linear-equations",
+  "level": "1",
+  "url": "sec-section-solving-non-linear-equations.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Solving Non-Linear Equations",
+  "body": " Solving Non-Linear Equations    "
+},
+{
+  "id": "sec-section-applications-of-equations",
+  "level": "1",
+  "url": "sec-section-applications-of-equations.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Applications of Equations",
+  "body": " Applications of Equations    "
+},
+{
+  "id": "sec-section-summary3",
+  "level": "1",
+  "url": "sec-section-summary3.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise3",
+  "level": "1",
+  "url": "sec-section-review-exercise3.html",
+  "type": "Section",
+  "number": "3.6",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "sec-section-revision-on-linear-inequalities-in-one-variable",
+  "level": "1",
+  "url": "sec-section-revision-on-linear-inequalities-in-one-variable.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Revision on Linear Inequalities in One Variable",
+  "body": " Revision on Linear Inequalities in One Variable    "
+},
+{
+  "id": "sec-section-systems-of-linear-inequalities-in-two-variables",
+  "level": "1",
+  "url": "sec-section-systems-of-linear-inequalities-in-two-variables.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Systems of Linear Inequalities in Two Variables",
+  "body": " Systems of Linear Inequalities in Two Variables    "
+},
+{
+  "id": "sec-section-inequalities-involving-absolute-value",
+  "level": "1",
+  "url": "sec-section-inequalities-involving-absolute-value.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Revision on Inequalities Involving Absolute Value",
+  "body": " Revision on Inequalities Involving Absolute Value    "
+},
+{
+  "id": "sec-section-quadratic-inequalities",
+  "level": "1",
+  "url": "sec-section-quadratic-inequalities.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "Revision on Quadratic Inequalities",
+  "body": " Revision on Quadratic Inequalities    "
+},
+{
+  "id": "sec-section-applications-of-inequalities",
+  "level": "1",
+  "url": "sec-section-applications-of-inequalities.html",
+  "type": "Section",
+  "number": "4.5",
+  "title": "Applications of Inequalities",
+  "body": " Applications of Inequalities    "
+},
+{
+  "id": "sec-section-summary4",
+  "level": "1",
+  "url": "sec-section-summary4.html",
+  "type": "Section",
+  "number": "4.6",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise4",
+  "level": "1",
+  "url": "sec-section-review-exercise4.html",
+  "type": "Section",
+  "number": "4.7",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "sec-section-revision-on-right-angled-triangles",
+  "level": "1",
+  "url": "sec-section-revision-on-right-angled-triangles.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Revision on Right Angled Triangles",
+  "body": " Revision on Right Angled Triangles    "
+},
+{
+  "id": "sec-section-trigonometric-ratios",
+  "level": "1",
+  "url": "sec-section-trigonometric-ratios.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Trigonometric Ratios",
+  "body": " Trigonometric Ratios    "
+},
+{
+  "id": "sec-section-summary5",
+  "level": "1",
+  "url": "sec-section-summary5.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise5",
+  "level": "1",
+  "url": "sec-section-review-exercise5.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "sec-section-sum-of-interior-angles-of-a-convex-polygon",
+  "level": "1",
+  "url": "sec-section-sum-of-interior-angles-of-a-convex-polygon.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Sum of Interior Angles of a Convex Polygon",
+  "body": " Sum of Interior Angles of a Convex Polygon    "
+},
+{
+  "id": "sec-section-sum-of-exterior-angles-of-a-convex-polygon",
+  "level": "1",
+  "url": "sec-section-sum-of-exterior-angles-of-a-convex-polygon.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Sum of Exterior Angles of a Convex Polygon",
+  "body": " Sum of Exterior Angles of a Convex Polygon    "
+},
+{
+  "id": "sec-section-measures-of-each-interior-angle-and-exterior-angle-of-a-regular-polygon",
+  "level": "1",
+  "url": "sec-section-measures-of-each-interior-angle-and-exterior-angle-of-a-regular-polygon.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Measures of Each Interior Angle and Exterior Angle of a Regular Polygon",
+  "body": " Measures of Each Interior Angle and Exterior Angle of a Regular Polygon    "
+},
+{
+  "id": "sec-section-properties-of-regular-polygons",
+  "level": "1",
+  "url": "sec-section-properties-of-regular-polygons.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "Properties of Regular Polygons",
+  "body": " Properties of Regular Polygons    "
+},
+{
+  "id": "sec-section-summary6",
+  "level": "1",
+  "url": "sec-section-summary6.html",
+  "type": "Section",
+  "number": "6.5",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise6",
+  "level": "1",
+  "url": "sec-section-review-exercise6.html",
+  "type": "Section",
+  "number": "6.6",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "sec-section-revision-on-congruency-of-triangles",
+  "level": "1",
+  "url": "sec-section-revision-on-congruency-of-triangles.html",
+  "type": "Section",
+  "number": "7.1",
+  "title": "Revision on Congruency of Triangles",
+  "body": " Revision on Congruency of Triangles    "
+},
+{
+  "id": "sec-section-definition-of-similar-figures",
+  "level": "1",
+  "url": "sec-section-definition-of-similar-figures.html",
+  "type": "Section",
+  "number": "7.2",
+  "title": "Definition of Similar Figures",
+  "body": " Definition of Similar Figures    "
+},
+{
+  "id": "sec-section-theorems-on-similar-plane-figures",
+  "level": "1",
+  "url": "sec-section-theorems-on-similar-plane-figures.html",
+  "type": "Section",
+  "number": "7.3",
+  "title": "Theorems on Similar Plane Figures",
+  "body": " Theorems on Similar Plane Figures    "
+},
+{
+  "id": "sec-section-ratio-of-perimeters-of-similar-plane-figures",
+  "level": "1",
+  "url": "sec-section-ratio-of-perimeters-of-similar-plane-figures.html",
+  "type": "Section",
+  "number": "7.4",
+  "title": "Ratio of Perimeters of Similar Plane Figures",
+  "body": " Ratio of Perimeters of Similar Plane Figures    "
+},
+{
+  "id": "sec-section-ratio-of-areas-of-similar-plane-figures",
+  "level": "1",
+  "url": "sec-section-ratio-of-areas-of-similar-plane-figures.html",
+  "type": "Section",
+  "number": "7.5",
+  "title": "Ratio of Areas of Similar Plane Figures",
+  "body": " Ratio of Areas of Similar Plane Figures    "
+},
+{
+  "id": "sec-section-construction-of-similar-plane-figure",
+  "level": "1",
+  "url": "sec-section-construction-of-similar-plane-figure.html",
+  "type": "Section",
+  "number": "7.6",
+  "title": "Construction of Similar Plane Figures",
+  "body": " Construction of Similar Plane Figures    "
+},
+{
+  "id": "sec-section-applications-of-similarities",
+  "level": "1",
+  "url": "sec-section-applications-of-similarities.html",
+  "type": "Section",
+  "number": "7.7",
+  "title": "Applications of Similarities",
+  "body": " Applications of Similarities    "
+},
+{
+  "id": "sec-section-summary7",
+  "level": "1",
+  "url": "sec-section-summary7.html",
+  "type": "Section",
+  "number": "7.8",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise7",
+  "level": "1",
+  "url": "sec-section-review-exercise7.html",
+  "type": "Section",
+  "number": "7.9",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "sec-section-vector-and-scalar-quantities",
+  "level": "1",
+  "url": "sec-section-vector-and-scalar-quantities.html",
+  "type": "Section",
+  "number": "8.1",
+  "title": "Vector and Scalar Quantities",
+  "body": " Vector and Scalar Quantities    "
+},
+{
+  "id": "sec-section-representation-of-a-vector",
+  "level": "1",
+  "url": "sec-section-representation-of-a-vector.html",
+  "type": "Section",
+  "number": "8.2",
+  "title": "Representation of a Vector",
+  "body": " Representation of a Vector   In this sub unit, we consider the vector represented by a directed line segment on a plane. This is called a vector on a plane.      A vector can be represented by either algebraically or geometrically. A vector is represented geometrically by a segment (a line segment with direction). In the vector is represented by a directed line segment and we denote this by an arrow as . In this case, the point is called the and the point is called the .         A vector also represented by using letters with an arrow bar over it such as , , or can also be a bold small letters like , , etc. Hence, the above vector represented by .    Equality of vectors   Two vectors are said to be if and only if they have the same magnitude and direction. In the two vectors have the same magnitude (length) and the same direction. Hence, they are equal vectors. In this case we can write this as , or .           Opposite of a vector  The vector which has the same magnitude as that of a vector but opposite in direction is called  and is denoted by From , you can observe that one vector is the negative of the other. Thus, as shown in the first part of and as shown in the second part of .               Parallel Vectors  Vectors that have the same or opposite direction are called     Identify parallel, equal and opposite vectors from below.                               and are in the same direction, is in opposite direction to and   Hence, these vectors and are parallel to each other.  There is no equal and opposite of a vector in the given figure.     and are in opposite directions but on the same line, so they are parallel.  Vectors and are also parallel since they are in the same direction.  There is no vector which is opposite of another in the given figure. since they have the same direction and equal length( use a ruler to check equality of length).              "
+},
+{
+  "id": "representation-of-vect",
+  "level": "2",
+  "url": "sec-section-representation-of-a-vector.html#representation-of-vect",
+  "type": "Figure",
+  "number": "8.2.1",
+  "title": "",
+  "body": "     "
+},
+{
+  "id": "equality-of-vectors",
+  "level": "2",
+  "url": "sec-section-representation-of-a-vector.html#equality-of-vectors",
+  "type": "Figure",
+  "number": "8.2.2",
+  "title": "",
+  "body": "     "
+},
+{
+  "id": "opposite-vector-1",
+  "level": "2",
+  "url": "sec-section-representation-of-a-vector.html#opposite-vector-1",
+  "type": "Figure",
+  "number": "8.2.3",
+  "title": "",
+  "body": "          "
+},
+{
+  "id": "subsec-parallel-vectors-3",
+  "level": "2",
+  "url": "sec-section-representation-of-a-vector.html#subsec-parallel-vectors-3",
+  "type": "Example",
+  "number": "8.2.4",
+  "title": "",
+  "body": "  Identify parallel, equal and opposite vectors from below.                               and are in the same direction, is in opposite direction to and   Hence, these vectors and are parallel to each other.  There is no equal and opposite of a vector in the given figure.     and are in opposite directions but on the same line, so they are parallel.  Vectors and are also parallel since they are in the same direction.  There is no vector which is opposite of another in the given figure. since they have the same direction and equal length( use a ruler to check equality of length).      "
+},
+{
+  "id": "exercises-parallel-vectors-1",
+  "level": "2",
+  "url": "sec-section-representation-of-a-vector.html#exercises-parallel-vectors-1",
+  "type": "Exercise",
+  "number": "8.2.3.1",
+  "title": "",
+  "body": "   "
+},
+{
+  "id": "sec-section-vector-operations",
+  "level": "1",
+  "url": "sec-section-vector-operations.html",
+  "type": "Section",
+  "number": "8.3",
+  "title": "Vector Operations",
+  "body": " Vector Operations    "
+},
+{
+  "id": "sec-section-position-vector",
+  "level": "1",
+  "url": "sec-section-position-vector.html",
+  "type": "Section",
+  "number": "8.4",
+  "title": "Position Vector",
+  "body": " Position Vector    "
+},
+{
+  "id": "sec-section-applications-of-vectors-in-two-dimensions",
+  "level": "1",
+  "url": "sec-section-applications-of-vectors-in-two-dimensions.html",
+  "type": "Section",
+  "number": "8.5",
+  "title": "Applications of Vectors in Two Dimensions",
+  "body": " Applications of Vectors in Two Dimensions    "
+},
+{
+  "id": "sec-section-summary8",
+  "level": "1",
+  "url": "sec-section-summary8.html",
+  "type": "Section",
+  "number": "8.6",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise8",
+  "level": "1",
+  "url": "sec-section-review-exercise8.html",
+  "type": "Section",
+  "number": "8.7",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "sec-section-statistical-data",
+  "level": "1",
+  "url": "sec-section-statistical-data.html",
+  "type": "Section",
+  "number": "9.1",
+  "title": "Statistical Data",
+  "body": " Statistical Data    "
+},
+{
+  "id": "sec-section-probability",
+  "level": "1",
+  "url": "sec-section-probability.html",
+  "type": "Section",
+  "number": "9.2",
+  "title": "Probability",
+  "body": " Probability    "
+},
+{
+  "id": "sec-section-summary9",
+  "level": "1",
+  "url": "sec-section-summary9.html",
+  "type": "Section",
+  "number": "9.3",
+  "title": "Summary",
+  "body": " Summary    "
+},
+{
+  "id": "sec-section-review-exercise9",
+  "level": "1",
+  "url": "sec-section-review-exercise9.html",
+  "type": "Section",
+  "number": "9.4",
+  "title": "Review Exercise",
+  "body": " Review Exercise    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
